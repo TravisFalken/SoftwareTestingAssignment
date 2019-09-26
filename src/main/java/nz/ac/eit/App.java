fddcbead4911b02f7e2544e9041ec0c30039dd74
Shannon Roberts <sneakypuppers@gmail.com>
2019-09-26 16:42:08 +1200

    fixed bug

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -29,20 +29,26 @@ public class App
 
         }
 
+        //rolls dice for player and added to hand value
+        MainGame.addValues(true);
+        //rolls dice for dealer and added to hand value
+
+            MainGame.addValues(false);
 
         while(end == false){
 
-           //rolls dice for player and added to hand value
-           MainGame.addValues(true);
-            //rolls dice for dealer and added to hand value
-            MainGame.addValues(false);
-           System.out.println("Your dice rolled: " + MainGame.getPlayer().handValue + "\n" +"Computer rolled: " +  MainGame.getDealer().getHandValue());
+
+            System.out.println("dice rolled:" + MainGame.getPlayer().getHandValue() + "\n" + "Computer rolled: " +  MainGame.getDealer().getHandValue());
+
 
 
             System.out.println("Would you like to hit or hold? Please enter 'hit' or 'hold'");
             userinput = sc.nextLine();
             if (userinput.equalsIgnoreCase("hit")) {
                 MainGame.addValues(true);
+                if(MainGame.getDealer().canBet()){
+                    MainGame.addValues(false);
+                }
                 if(MainGame.checkPlayerBust()){
                     System.out.println("Your dice rolled: " + MainGame.getPlayer().handValue + "\n" +"Computer rolled: " +  MainGame.getDealer().getHandValue());
                     System.out.println(MainGame.DealerWin());
@@ -52,13 +58,21 @@ public class App
                     System.out.println(MainGame.PlayerWin());
                     end = true;
                 }
+
             } else if (userinput.equalsIgnoreCase("hold")) {
+                if(MainGame.getDealer().canBet()){
+                    MainGame.addValues(false);
+                }
                 if(MainGame.determineWin() == 1) {
                     System.out.println(MainGame.PlayerWin());
+                    System.out.println("Your dice rolled: " + MainGame.getPlayer().handValue + "\n" +"Computer rolled: " +  MainGame.getDealer().getHandValue());
+
                     end = true;
 
                 } else if (MainGame.determineWin() == 0) {
                     System.out.println(MainGame.DealerWin());
+                    System.out.println("Your dice rolled: " + MainGame.getPlayer().handValue + "\n" +"Computer rolled: " +  MainGame.getDealer().getHandValue());
+
                     end = true;
                 }
             } else {
