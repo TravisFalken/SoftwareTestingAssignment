35a40e05d21653a368d477766ee07ded06d5f498
stacey moana <staaacey84@hotmail.com>
2019-10-08 12:10:50 +1300

    commented out increasehandvalue (not needed)

--- src/main/java/nz/ac/eit/Player.java
+++ src/main/java/nz/ac/eit/Player.java
@@ -38,17 +38,18 @@ public class Player {
         this.handValue = handValue;
     }
 
-    public void increaseHandValue(int roundScore) {
-        this.handValue = roundScore;
-    }
+//    public void increaseHandValue(int roundScore) {
+//        this.handValue = roundScore;
+//    }
 
 //    //Increase the game score each time the user wins a round
 //    public void increaseGameScore(int gameScore) {
 //        this.gameScore++;
 //    }
 
-    //resets the handValue at the beginning of every round
-    public void resetHandValue(){
-        this.handValue = 0;
-    }
+//    //resets the handValue at the beginning of every round
+//    public void resetHandValue(){
+//        this.handValue = 0;
+//    }
+
 }
