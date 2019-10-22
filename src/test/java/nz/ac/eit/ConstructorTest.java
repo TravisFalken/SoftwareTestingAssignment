ec42e7f78e6acba4f0da1702107fb0b69082cddd
stacey moana <staaacey84@hotmail.com>
2019-10-22 14:03:19 +1300

    Added constructor tests

---
+++ src/test/java/nz/ac/eit/ConstructorTest.java
@@ -0,0 +1,37 @@
+package nz.ac.eit;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+
+public class ConstructorTest {
+    private Player player;
+    private Die die;
+    private Game game;
+    private Dealer dealer;
+
+    @Test
+    public void playerTest_Construction_True() {
+        player = new Player("Timmy Turner");
+        assertThat(player, instanceOf(Player.class));
+    }
+
+    @Test
+    public void dieTest_Construction_True() {
+        die = new Die();
+        assertThat(die, instanceOf(Die.class));
+    }
+
+    @Test
+    public void gameTest_Construction_True() {
+        game = new Game();
+        assertThat(game, instanceOf(Game.class));
+    }
+
+    @Test
+    public void dealerTest_Construction_True() {
+        dealer = new Dealer();
+        assertThat(dealer, instanceOf(Dealer.class));
+    }
+}
